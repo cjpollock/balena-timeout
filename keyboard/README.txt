fd4ddc4527e464e998d2ec20c760847a1d996f68
CJ Pollock <cpollock@deervalley.com>
2019-09-06 17:25:05 -0600

    Added comments.

---
+++ keyboard/README.txt
@@ -0,0 +1,2 @@
+Using the python-keyboard package, this container will listen for keyboard input and reset a specified container after a period of time.
+Thanks to the creators of python-keyboard and the Balena team!
--- keyboard/start.py
+++ keyboard/start.py
@@ -5,26 +5,22 @@ sys.path.append("...")
 import keyboard
 
 print('timer started...')
-timeoutTimer = 60
+timeoutTimer = 60 # Sets the initial timer. 60 = 60 minutes.
 print('Starting session with %d minutes.'%timeoutTimer)
-
-keyboard.write('num_lock')
 
 def on_press_reaction(e):
     global timeoutTimer
-    timeoutTimer = 3
+    timeoutTimer = 3 # Resets the timer for 3 minutes for every keyboard input.
     print('Keyboard input detected; session reset to %d minutes.'%timeoutTimer)
 
 while timeoutTimer>0:
     global timeoutTimer
     keyboard.hook(on_press_reaction)
-    time.sleep(60)
-    timeoutTimer -= 1
+    time.sleep(60) # this is what sets the time in seconds. 60 = 1 minute.
+    timeoutTimer -= 1 #subtracts 1 minute from the timer.
     print('Minutes remaining in session: %d'%timeoutTimer)
 else:
     print("Restarting browser")
     os.system('curl --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/restart-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d \'{"serviceName": "chromium-kiosk"}\'')
-    timeoutError = 60
-    #time.sleep(10)
-    #print("Restarting keyboard")
-    #os.system('curl --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/restart-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d \'{"serviceName": "keyboard"}\'')
+    # In the previous line of code, change {"serviceName": "chromium-kiosk"} to the name of the container you want to restart.
+    # Example: {"serviceName": "my-awesome-container"}
